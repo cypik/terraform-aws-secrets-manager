491cb1bc1ee3fe6308208ff7a665d18eb418987a
Sohan Yadav <sohanyadavdevops@gmail.com>
2026-04-15 15:59:56 +0530

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 locals {
   secrets = [
-    for secret in var.secrets : {
-      name                    = lookup(secret, "name", null)
-      name_prefix             = lookup(secret, "name_prefix", null)
-      description             = lookup(secret, "description", null)
-      kms_key_id              = lookup(secret, "kms_key_id", null)
-      policy                  = lookup(secret, "policy", null)
-      replica                 = lookup(secret, "replica", null)
-      recovery_window_in_days = lookup(secret, "recovery_window_in_days", var.recovery_window_in_days)
-      secret_string = lookup(secret, "secret_string", null) != null ? lookup(secret, "secret_string", null) : (
-        lookup(secret, "secret_key_value", null) != null ? jsonencode(lookup(secret, "secret_key_value", {})) : null
-      )
-      secret_binary = lookup(secret, "secret_string", null) == null ? lookup(secret, "secret_binary", null) : null
-      tags          = lookup(secret, "tags", {})
-    }
+  for secret in var.secrets : {
+    name                    = lookup(secret, "name", null)
+    name_prefix             = lookup(secret, "name_prefix", null)
+    description             = lookup(secret, "description", null)
+    kms_key_id              = lookup(secret, "kms_key_id", null)
+    policy                  = lookup(secret, "policy", null)
+    replica                 = lookup(secret, "replica", null)
+    recovery_window_in_days = lookup(secret, "recovery_window_in_days", var.recovery_window_in_days)
+    secret_string = lookup(secret, "secret_string", null) != null ? lookup(secret, "secret_string", null) : (
+    lookup(secret, "secret_key_value", null) != null ? jsonencode(lookup(secret, "secret_key_value", {})) : null
+    )
+    secret_binary = lookup(secret, "secret_string", null) == null ? lookup(secret, "secret_binary", null) : null
+    tags          = lookup(secret, "tags", {})
+  }
   ]
 }
 
@@ -51,7 +51,7 @@ resource "aws_secretsmanager_secret" "main" {
 
 resource "aws_secretsmanager_secret_version" "sm-sv" {
   count         = var.unmanaged && var.enabled ? 0 : length(local.secrets)
-  secret_id     = aws_secretsmanager_secret.main[0].id
+  secret_id     = aws_secretsmanager_secret.main[count.index].id
   secret_string = lookup(element(local.secrets, count.index), "secret_string", null)
   secret_binary = lookup(element(local.secrets, count.index), "secret_binary", null) != null ? base64encode(lookup(element(local.secrets, count.index), "secret_binary", null)) : null
   depends_on    = [aws_secretsmanager_secret.main]
@@ -59,7 +59,7 @@ resource "aws_secretsmanager_secret_version" "sm-sv" {
 
 resource "aws_secretsmanager_secret_version" "sm-svu" {
   count         = var.unmanaged && var.enabled ? length(local.secrets) : 0
-  secret_id     = aws_secretsmanager_secret.main[*].id[count.index]
+  secret_id     = aws_secretsmanager_secret.main[count.index].id
   secret_string = lookup(element(local.secrets, count.index), "secret_string", null)
   secret_binary = lookup(element(local.secrets, count.index), "secret_binary", null) != null ? base64encode(lookup(element(local.secrets, count.index), "secret_binary", null)) : null
   depends_on    = [aws_secretsmanager_secret.main]
@@ -74,9 +74,9 @@ resource "aws_secretsmanager_secret_version" "sm-svu" {
 }
 
 resource "aws_secretsmanager_secret_rotation" "main" {
-  count               = var.enabled && var.enable_rotation ? 1 : 0
+  count               = var.enabled && var.enable_rotation ? length(local.secrets) : 0
   rotation_lambda_arn = var.rotation_lambda_arn
-  secret_id           = aws_secretsmanager_secret.main[*].id
+  secret_id           = aws_secretsmanager_secret.main[count.index].id
 
   dynamic "rotation_rules" {
     for_each = [var.rotation_rules]
